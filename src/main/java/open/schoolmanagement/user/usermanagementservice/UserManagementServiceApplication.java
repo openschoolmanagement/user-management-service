c31881292377683df15de72c49322fdb6b69ad2c
Philipp <philippkostyra@gmail.com>
2018-05-26 18:25:31 +0200

    Intial Spring Boot Setup
    Copy of Initialzr Files

---
+++ src/main/java/open/schoolmanagement/user/usermanagementservice/UserManagementServiceApplication.java
@@ -0,0 +1,12 @@
+package open.schoolmanagement.user.usermanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementServiceApplication.class, args);
+	}
+}
