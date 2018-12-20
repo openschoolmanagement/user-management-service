d559390158fa299804b56e643e78f4143bea9df8
Thomas Bonk <thomas@meandmymac.de>
2018-12-20 21:48:16 +0100

    reverted to the non generated server

--- build.ts
+++
@@ -1,8 +0,0 @@
-import s from 'shelljs';
-const outDir = 'dist'
-
-s.rm('-rf', outDir);
-s.mkdir(outDir);
-s.cp('.env', `${outDir}/.env`);
-s.mkdir('-p', `${outDir}/common/swagger`);
-s.cp('server/common/swagger/Api.yaml', `${outDir}/common/swagger/Api.yaml`);
